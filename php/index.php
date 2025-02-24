7484738b00c6c042307f0b23f36e4f7a2fb4dd58
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-02-24 21:33:14 +0900

    mysql & php 연결

--- php/index.php
+++ php/index.php
@@ -1,3 +1,7 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '111111', 'mysql');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,12 +13,14 @@
 <body>
     <div class="app">
         <div class="container">
-            <div id="wrapper">
-                <input type="text" placeholder="할 일을 입력하세요"/>
-                <div id="add-btn">
-                    <input type="submit" value="Add">
+            <form method="post" action="">
+                <div id="wrapper">
+                    <input type="text" placeholder="할 일을 입력하세요"/>
+                    <div id="add-btn">
+                        <input type="submit" value="Add" name="add-todo">
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
         <p id="error">빈 칸으로 작성할 수 없습니다.</p>
     </div>
