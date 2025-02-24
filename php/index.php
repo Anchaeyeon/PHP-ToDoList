7001b58fa104c8187b8f3f9ee2849e9d9158f56b
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-02-24 22:23:32 +0900

    사용자가 입력한 글 db에 저장

--- php/index.php
+++ php/index.php
@@ -1,5 +1,13 @@
 <?php
 $conn = mysqli_connect('localhost', 'root', '111111', 'mysql');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $todo = $_POST['add_todo'];
+
+    // 데이터 삽입
+    $insertTodo = "insert into todolist (todo) values ('$todo')";
+    mysqli_query($conn, $insertTodo);
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,15 +23,14 @@ $conn = mysqli_connect('localhost', 'root', '111111', 'mysql');
         <div class="container">
             <form method="post" action="">
                 <div id="wrapper">
-                    <input type="text" placeholder="할 일을 입력하세요"/>
+                    <input type="text" name="add_todo" placeholder="할 일을 입력하세요"/>
                     <div id="add-btn">
-                        <input type="submit" value="Add" name="add-todo">
+                        <input type="submit" value="Add">
                     </div>
                 </div>
             </form>
         </div>
         <p id="error">빈 칸으로 작성할 수 없습니다.</p>
     </div>
-    <script src="../js/index.js"></script>
 </body>
 </html>
