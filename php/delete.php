6c9f2c11a769dc8f3c6480548753c24ad8ae87a6
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-02-25 01:09:27 +0900

    선택한 투두리스트 삭제하기

--- php/delete.php
+++ php/delete.php
@@ -11,4 +11,15 @@ if (!$row) {
     echo "todolist를 찾을 수 없습니다.";
     exit;
 }
+
+$todo_delete = "delete from todolist where id = $id";
+$final_delete = mysqli_query($conn, $todo_delete);
+
+if ($final_delete) {
+    echo "<script>alert('todolist가 삭제되었습니다!');</script>";
+    echo("<script>location.href='index.php';</script>");
+}
+else {
+    echo "<script>alert('todolist 삭제 중 오류가 발생했습니다.');</script>";
+}
 ?>
